e88ad05c9b2555e64a2e568a461bbab94aac5ac7
aOvOb <junysuh@gmail.com>
2021-06-11 00:21:55 +0900

    0610

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+# Junytube
+
+/ -> Home
+
+
+/users/join -> Join
+/users/login -> Login
+/users/edit -> Edit Profile
+/users/delete -> Delete Profile
+
+/videos/search
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete a Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,36 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = (req, res, next) => {
-    next();
-}
 
-const routerLogger = (req, res, next) => {
-    console.log("PATH", req.path);
-    next();
-};
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
-const methodLogger = (req, res, next) => {
-    console.log("METHOD", req.method);
-    next();
-};
-
-const home = (req, res, next) => {
-    console.log("im the last one")
-    res.send("hello"); 
-    next();
-};
-
-const login = (req, res, next) => {
-    return res.send("Login");
-};
-
-app.use(methodLogger, routerLogger);
-app.get("/", home);
-app.get("/login", login);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} ✔`);
 
