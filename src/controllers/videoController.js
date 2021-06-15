3a644d1582f91d9d6de2e3052bc5420f99f380d7
aOvOb <junysuh@gmail.com>
2021-06-16 01:31:59 +0900

    im sleepy...

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -46,6 +46,16 @@ export const postEdit = (req, res) => {
   videos[id -1].title = title;
   return res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  return res.render("/");
+};
+
+
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Vidoe");
