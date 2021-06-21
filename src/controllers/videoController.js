6065f78b3c54a0b23057b270e9b6b7339770813b
juny <junysuh@gmail.com>
2021-06-21 17:13:24 +0900

    062121

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,15 +9,24 @@ const handelSearch = (error, document) => {
 
 export const home = async(req, res) => {
   Video.find({}, (error, videos) => {
-    res.render("home", { pageTitle: "Home", videos:[] });
+    res.render("home", { pageTitle: "Home", videos });
   });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;  
-  return res.render("watch", { pageTitle: `Watching`});
+  const video = await Video.findById(id);
+  if(video){
+    return res.render("watch", { pageTitle: video.title, video });
+  }
+  return res.render("404", { pageTitle: "Video not found" });  
 };
-export const getEdit = (req, res) => {
-  return res.render("edit", { pageTitle: "Edit" });
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if(!video) {
+    return res.render("404", { pageTitle: "Video note found."});
+  }
+  return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
