47a2a3cf7042224b636d711d32f77f5a74951ad0
juny <junysuh@gmail.com>
2021-09-14 22:38:40 +0900

    091421

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,22 @@
 import User from "../models/User";
+import bcrypt from "bcrypt"
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if(password !== password2) {
+    return res.status(400).render("Join", { pageTitle, errorMessage: "Password confirmation does not match."})
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if(exists) {
+    return res.status(400).render("join", { pageTitle, errorMessage: "This Username/email is already taken."})
+  }
+  // $or 로 대체
+  // const emailExists = await User.exists({ email });
+  // if(emailExists) {
+  //   return res.render("join", { pageTitle, errorMessage: "This email is already taken."})
+  // }
   await User.create({
   name,
   username,
@@ -17,15 +31,17 @@ export const getLogin = (req, res) =>
 
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const exists = await User.exists({ username });
-  if (!exists) {
-    return res.status(400).render("login", {
-      pageTitle: "Login",
-      errorMessage: "An account with this username does not exists.",
-    });
+  const pageTitle = "Login"
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", { pageTitle, errorMessage: "An account with this username does not exists.", });
   }
-  // check if password correct
-  res.end();
+  const ok = await bcrypt.compare(password, user.password);
+  if(!ok){
+    return res.status(400).render("login", { pageTitle, errorMessage: "Wrong Password.", });
+  }
+  console.log("Login Success!")
+  return res.redirect("/");
 };
 
 // export const join = (req, res) => res.render("createAccount", { pageTitle: "Create Account" });
