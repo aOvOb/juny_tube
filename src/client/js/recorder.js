a7f4926b4df5eeb9429758fe6be20ae1029a018d
aOvOb <junysuh@gmail.com>
2021-11-20 20:24:48 +0900

    ffmpeg deleted

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,20 +1,11 @@
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
-
 const startBtn = document.getElementById("startBtn");
-const video = document.getElementById("preview")
+const video = document.getElementById("preview");
 
 let stream;
 let recorder;
-let videoFile; 
+let videoFile;
 
-const handleDownload = async () => {
-  const ffmpeg = createFFmpeg({ log: true });
-  await ffmpeg.load();
-
-  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
-
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
-
+const handleDownload = () => {
   const a = document.createElement("a");
   a.href = videoFile;
   a.download = "MyRecording.webm";
@@ -23,37 +14,33 @@ const handleDownload = async () => {
 };
 
 const handleStop = () => {
-  startBtn.innerText = "Download Recording"
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
   startBtn.addEventListener("click", handleDownload);
-
   recorder.stop();
-}
+};
 
 const handleStart = () => {
-  startBtn.innerText = "Stop Recording"
+  startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-
-  recorder = new MediaRecorder(stream)
+  recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
     video.play();
-  }
+  };
   recorder.start();
 };
 
-
-
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+    audio: false,
     video: true,
   });
-  video.srcObject = stream
+  video.srcObject = stream;
   video.play();
 };
 
