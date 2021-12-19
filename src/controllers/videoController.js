ce79a5a21715f387f79413c1609f792438c60a3e
aOvOb <junysuh@gmail.com>
2021-12-19 23:06:15 +0900

    made thumbnail

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -61,13 +61,15 @@ export const postUpload = async (req, res) => {
   const {
     user: { _id },
   } = req.session;
-  const { path: fileUrl } = req.file;
+  const { video, thumb } = req.files;
+  console.log(video, thumb);
   const { title, description, hashtags } = req.body;
   try {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl,
+      fileUrl: video[0].path,
+      thumbUrl: thumb[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
@@ -117,7 +119,7 @@ export const registerView = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.sendStatus(404)
+    return res.sendStatus(404);
   }
   video.meta.views = video.meta.views + 1;
   await video.save();
